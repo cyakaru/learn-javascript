5cf5ae93442037b61dae26a6d7153ed6c51c3a3d
Shafwan Khairullah <shafwankhairullah35@gmail.com>
2025-01-22 11:40:53 +0700

    Belajar Javascript

--- modularitation/anotherfile.mjs
+++ modularitation/anotherfile.mjs
@@ -1,8 +1,19 @@
+/*
+Export adalah keyword yang kita gunakan untuk melabeli suatu function/method/variable agar dapat diakses dari luar modul saat ini. Export terdiri dari dua jenis, yaitu default export dan named export.
+*/
+
+// Default Export
+// Default Export adalah metode yang mengexport function / method dsb hanya satu modul. Digunakana agar tidak bentrok, dan disarankan hanya satu modul saja.
 export default function myFuntion() {
   console.log("Ini adalah function pada anotherfile.mjs");
 }
 
+// Named Export
+// Named Export Setelah Deklarasi
 const name = "Awan";
 const age = 17;
 
 export { name, age };
+
+// Named Export Sebelum Deklarasi
+export const school = "SMK Negeri 2 Dumai";
--- modularitation/main.mjs
+++ modularitation/main.mjs
@@ -5,7 +5,7 @@ Import dapat dilakukan dengan syarat :
 Import dapat dilakukan dengan menulis kata kunci import kemudian diikuti oleh nama function/method/variable.
 Yang kita import adalah default import karena pada file anotherfile.mjs nya myFunction dibuat sebagai default eksport maka ketika kita mengimpor dengan nama apapun, yang keluar adalah myFunction() karena defaultnya itu.
 */
-import myFuntion from "./anotherfile.mjs";
+import myFuntion from "./anotherfile.mjs"; // <- Mengimpor default export tidak perlu {} dan namanya boleh bebas. Kita dapat menggabungkan default dengan named, tetapi tidak baik dilakukan, dengan cara default, {named}
 myFuntion();
 
 /*
@@ -15,3 +15,20 @@ Selain default import, ada namanya named import, ketika mengimport function atau
 import { name, age } from "./anotherfile.mjs";
 console.log(name);
 console.log(age);
+
+/*
+Cara lain menggunakan import adalah menggunakan import * ketika kita memiliki banyak variabel/method/funcion yang ingin di import
+*/
+
+import * as variable from "./anotherfile.mjs";
+console.log(variable.name);
+console.log(variable.age);
+
+/*
+as digunakan untuk memberikan nama alias pada function/method yang kita import, ini berguna untuk mengatasi nama function/method yang sama di kedua file yang berbeda.
+*/
+
+import { name as nameFromAnother } from "./anotherfile.mjs";
+import { name as nameFromAnotherAgain } from "./anotherfileagain.mjs";
+console.log(nameFromAnother);
+console.log(nameFromAnotherAgain);
