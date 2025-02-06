f2ee838ee12ab9a4c79c8662bf2aaceb7aab1a87
Shafwan Khairullah <shafwankhairullah35@gmail.com>
2025-02-06 21:18:44 +0700

    Belajar Javascript

--- final-assessment/optional-04-oop/Inventory.js
+++ final-assessment/optional-04-oop/Inventory.js
@@ -8,7 +8,18 @@
  */
 
 class Inventory {
-  constructor(items, addItem, removeItem, listItems) {}
+  constructor() {
+    this.items = [];
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(id) {
+    this.items = this.items.filter((item) => item.id !== id);
+  }
+  listItems() {
+    return this.items.map((item) => item.displayDetails()).join("\n");
+  }
 }
 
 // Jangan hapus kode di bawah ini!
